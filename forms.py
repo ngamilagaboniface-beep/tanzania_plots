896f70e9e026961fd79f8cccd2afebc6ed4082d0
ngamilagaboniface-beep <ngamilagaboniface@gmail.com>
2026-04-22 11:33:36 +0300

    Refactor forms and add PasswordChangeForm

--- forms.py
+++ forms.py
@@ -1,27 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, FloatField, IntegerField, SelectField, PasswordField, SubmitField, MultipleFileField
-from wtforms.validators import DataRequired, EqualTo, Optional
+from wtforms import String_Field, PasswordField, TextAreaField, FloatField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = String_Field('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
-
-class PlotForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[DataRequired()])
-    price = FloatField('Price (TZS)', validators=[DataRequired()])
-    location = StringField('Location', validators=[DataRequired()])
-    sqm_size = IntegerField('Size (Sq.m.)', validators=[DataRequired()])
-    status = SelectField('Status', choices=[('Available', 'Available'), ('Sold', 'Sold')], default='Available')
-    images = MultipleFileField('Upload Images (select multiple)', validators=[Optional()])
-    submit = SubmitField('Save Plot')
 
 class PasswordChangeForm(FlaskForm):
-    old_password = PasswordField('Old Password', validators=[DataRequired()])
-    new_password = PasswordField('New Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm New Password', validators=[
-        DataRequired(),
-        EqualTo('new_password', message='New passwords must match')
-    ])
-    submit = SubmitField('Change Password')
+    old_password = PasswordField('Current Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('new_password')])
+
+class PlotForm(FlaskForm):
+    title = String_Field('Property Title', validators=[DataRequired()])
+    location = String_Field('Location (e.g. Goba)', validators=[DataRequired()])
+    price = FloatField('Price (TZS)', validators=[DataRequired()])
+    sqm_size = String_Field('Size (Sqm)', validators=[DataRequired()])
+    status = SelectField('Status', choices=[('Available', 'Available'), ('Sold', 'Sold')])
+    description = TextAreaField('Full Description')
